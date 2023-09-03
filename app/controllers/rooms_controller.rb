fa3ed9b2aa1da3c4d8ebbe492fdbd2a4872709c3
kotomiyashita <kotomi.owo@gmail.com>
2023-09-04 02:48:27 +0900

    複数人追加機能実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,7 +8,16 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
+    user_ids = params[:room][:user_ids].split(',').map(&:to_i) # 文字列から整数に変換
+    selected_users = User.where(id: user_ids).to_a
+  
+    # current_user を選択されたユーザーに追加
+    selected_users << current_user
+  
     if @room.save
+      # 選択されたユーザーをチャットルームのメンバーに追加
+      @room.users = selected_users  # ここで users リレーションを上書き
+  
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
