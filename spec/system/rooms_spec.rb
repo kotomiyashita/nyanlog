dd7484a3ddbccd3eb468d6b5d519e224547fd9da
kotomiyashita <kotomi.owo@gmail.com>
2023-09-05 18:24:23 +0900

    チャットルーム削除の結合テストを追加

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -52,3 +52,25 @@ RSpec.describe 'チャットルーム新規作成', type: :system do
     end
   end
 end
+
+RSpec.describe 'チャットルームの削除機能', type: :system do
+  before do
+    basic_pass root_path
+    @room_user = FactoryBot.create(:room_user)
+    sign_in(@room_user.user)
+  end
+
+  it 'チャットルームを削除すると、関連するメッセージがすべて削除されている' do
+    # 作成されたチャットルームへ遷移する
+    click_on(@room_user.room.name)
+    # メッセージ情報を5つDBに追加する
+    FactoryBot.create_list(:message, 5, room_id: @room_user.room.id, user_id: @room_user.user.id)
+    # 「チャットを終了する」ボタンをクリックすることで、作成した5つのメッセージが削除されていることを確認する
+    expect {
+      find_link('チャットを終了する',  href: room_path(@room_user.room)).click
+      sleep 1
+    }.to change { @room_user.room.messages.count }.by(-5)
+    # トップページに遷移していることを確認する
+    expect(current_path).to eq(root_path)
+  end
+end
